943194cccb24cb7bf9ebab233cd882a5404635de
RodriguezIA <edgaralanra@gmail.com>
2022-09-17 01:01:15 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const typed = new Typed('.typed', {
+    strings: [
+        '<h1>hi, <span id="name">edgar<span> here.</h1>',
+    ],
+    stringElement: '#cadenas-texto',
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    smartBackspace: true,
+    shuffle: false,
+    backDelay: 1500,
+    loop: false,
+    loopCount: false,
+    cursorChar: ' ',
+    contentType: 'html'
+});
